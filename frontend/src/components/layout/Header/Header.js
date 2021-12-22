d2f88f27d1c3cb944239d6e623519afeb858b41f
Mihir Baidya <mihirbaidya08@gmail.com>
2021-12-23 01:08:47 +0530

    add UI of registar/login page

--- frontend/src/components/layout/Header/Header.js
+++ frontend/src/components/layout/Header/Header.js
@@ -26,6 +26,7 @@ const options = {
     nav4justifyContent: "flex-start",
     link1ColorHover: "#eb4034",
     link1Margin: "1vmax",
+    profileIconUrl: "/login",
     profileIconColor: "rgba(35,35,35,0.8)",
     searchIconColor: "rgba(35,35,35,0.8)",
     cartIconColor: "rgba(35,35,35,0.8)",
