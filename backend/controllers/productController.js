a86638bba5a52f33ea4a66fa53496ce622c62907
Mihir Baidya <mihirbaidya08@gmail.com>
2021-12-22 21:42:32 +0530

    fix error

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,12 +23,19 @@ exports.getAllProducts = catchAsyncErrors(async (req, res, next) => {
         .search()
         .filter()
         .pagination(resultPerPage);
+
+    // let products = await apiFeature.query;
+    // let filteredProductsCount = products.length;
+    // apiFeature.pagination(resultPerPage);
+
     const products = await apiFeature.query;
 
     res.status(200).json({
         success: true,
         products,
         productsCount,
+        resultPerPage,
+        // filteredProductsCount,
     });
 });
 
