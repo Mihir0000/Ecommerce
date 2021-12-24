723348c7bd8f4cc0790658125bf6c22242668ccb
Mihir Baidya <mihirbaidya08@gmail.com>
2021-12-25 01:03:25 +0530

    information not provide by developer

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -81,16 +81,14 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
     await user.save({ validateBeforeSave: false });
 
-    const resetPasswordUrl = `${req.protocol}://${req.get(
-        "host"
-    )}/api/v1/password/reset/${resetToken}`;
+    const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`;
 
-    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have nor requested this email then, please ignore it.`;
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
 
     try {
         await sendEmail({
             email: user.email,
-            subject: `Ecommerce Password Recovery`,
+            subject: `Ecommerce (Baidya Store) Password Recovery`,
             message,
         });
         res.status(200).json({
