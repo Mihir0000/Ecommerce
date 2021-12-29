8c9a29e1e29037f0e5d7d7af3c485a25a9466736
Mihir Baidya <mihirbaidya08@gmail.com>
2021-12-29 13:09:30 +0530

    add payment features

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
 
 const app = express();
+
+// config
+dotenv.config({ path: "backend/config/config.env" });
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,10 +19,12 @@ app.use(fileUpload());
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
 const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+app.use("/api/v1", payment);
 
 // Middleware for error
 app.use(errorMiddleware);
